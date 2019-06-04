2c269124840e6956f4e6c77d023c152a1e56d253
Islam Mansour <islam.alam.aerospace@gmail.com>
2019-06-04 07:12:57 +0200

    Update 03_conda_env.sh

--- 03_conda_env.sh
+++ 03_conda_env.sh
@@ -2,6 +2,23 @@
 
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
+conda install -c -y conda-forge nodejs   # or some other way to have a recent node
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+jupyter labextension install ipyvolume
+jupyter labextension install jupyter-threejs
+
+conda install -c -y conda-forge ipyleaflet
+jupyter labextension install jupyter-leaflet
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
+# Pip install Leaflet and ipyvolume
+#apt install -y python3-pip
+
+#pip3 install ipyleaflet
+#jupyter nbextension enable --py --sys-prefix ipyleaflet  # can be skipped for notebook 5.3 and above
+#jupyter labextension install jupyter-leaflet
+#jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
 # Go to home directory
 # cd ~
 source /opt/anaconda/etc/profile.d/conda.sh
