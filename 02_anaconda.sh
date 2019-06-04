2cd5627194d808ec4c80336d89aa7f568c053492
Islam Mansour <islam.alam.aerospace@gmail.com>
2019-06-04 06:22:54 +0200

    Update 02_anaconda.sh

--- 02_anaconda.sh
+++ 02_anaconda.sh
@@ -3,6 +3,7 @@
 cd ~
 
 # https://stackoverflow.com/questions/22510705/get-the-latest-download-link-programmatically
+# https://docs.anaconda.com/anaconda/install/silent-mode/
 
 # Determine latest version:
 latestVer=$(curl 'https://repo.anaconda.com/archive/' | 
@@ -16,27 +17,47 @@ curl "https://repo.anaconda.com/archive/Anaconda3-${latestVer}-Linux-x86_64.sh" 
 
 # curl "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" > Anaconda3-latest-Linux-x86_64.sh
 
-bash Anaconda3-latest-Linux-x86_64.sh -b -p /opt/anaconda
+# Installing in silent mode
+bash Anaconda3-latest-Linux-x86_64.sh -f -b -p /opt/anaconda
+
+# To run the silent installation of Miniconda for macOS or Linux, specify the -b and -p arguments of the bash installer. The following arguments are supported:
+
+# -b—Batch mode with no PATH modifications to ~/.bashrc. Assumes that you agree to the license agreement. Does not edit the .bashrc or .bash_profile files.
+# -p—Installation prefix/path.
+# -f—Force installation even if prefix -p already exists.
+
+
 rm Anaconda3-latest-Linux-x86_64.sh
-# echo '. /opt/anaconda/etc/profile.d/conda.sh' >> ~/.bashrc 
 
-sudo ln -s /opt/anaconda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
-source /etc/profile.d/conda.sh
+# The installer will not prompt you for anything, including setup of your shell to activate conda. To add this activation in your current shell session:
+eval "$(/opt/anaconda/bin/conda shell.bash hook)"
 
-# Refresh basically
-source $HOME/.bashrc
+# With this activated shell, you can then install conda’s shell functions for easier access in the future:
 conda init
 
-# Refresh basically
-source $HOME/.bashrc
+# If you’d prefer that conda’s base environment not be activated on startup, set the auto_activate_base parameter to false:
+
 conda config --set auto_activate_base false
 
-# Refresh basically
-source $HOME/.bashrc
-
 conda update conda
+# echo '. /opt/anaconda/etc/profile.d/conda.sh' >> ~/.bashrc 
+
+# sudo ln -s /opt/anaconda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
+# source /etc/profile.d/conda.sh
+
+# Refresh basically
+# source $HOME/.bashrc
+# conda init
+
+# Refresh basically
+# source $HOME/.bashrc
+
+# Refresh basically
+# source $HOME/.bashrc
+
+
 #conda install -c anaconda anaconda-navigator 
 
-source /etc/profile.d/conda.sh
+# source /etc/profile.d/conda.sh
 
 echo "Installation is complete :)"
